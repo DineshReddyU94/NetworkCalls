a47271740254fdf65687e75c49109bab0fd53cbe
Dinesh reddy <udhyaldinesh94@gmail.com>
2024-03-05 08:57:19 +0530

    Network Calls Initial Commit

---
+++ NetworkCalls/NetworkCalls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkCalls
+//
+//  Created by Dinesh Reddy on 05/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
